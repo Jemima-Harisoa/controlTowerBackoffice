82351fd582d3bbc68621f1826c7f492e8843cfb8
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-02-06 11:07:34 +0300

    build du projet

---
+++ build.bat
@@ -0,0 +1,4 @@
+.\build.ps1 compile     # Compilation rapide
+.\build.ps1 package     # Création du fichier WAR
+.\build.ps1 test        # Exécution des tests  
+.\build.ps1 full        # Build complet
